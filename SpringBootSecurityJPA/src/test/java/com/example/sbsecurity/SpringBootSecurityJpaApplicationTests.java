9769852a3bc7a345f4a9af307e49316237fab4e8
nguyenbakhoi <nguyenbakhoi.hust@gmail.com>
2022-07-14 14:58:48 +0700

    First Project in 1.JavaSpring

---
+++ SpringBootSecurityJPA/src/test/java/com/example/sbsecurity/SpringBootSecurityJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
