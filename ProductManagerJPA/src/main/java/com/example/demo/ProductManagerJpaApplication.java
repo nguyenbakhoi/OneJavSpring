5ef32aecce65db201bf5c44f25dc488dea9b7280
nguyenbakhoi <nguyenbakhoi.hust@gmail.com>
2022-07-19 08:49:42 +0700

    Post project ProductManagerJPA

---
+++ ProductManagerJPA/src/main/java/com/example/demo/ProductManagerJpaApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagerJpaApplication {
+
+	//https://www.youtube.com/watch?v=W-rEIR8KHWY&ab_channel=Nguy%E1%BB%85nB%C3%A1Kh%E1%BB%9Fi
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagerJpaApplication.class, args);
+	}
+
+}
